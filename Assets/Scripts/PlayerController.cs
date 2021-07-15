1ab931bd5f0eeab98e58844825d25c42f6cd785a
Neil <neil@premiergsolutions.com>
2021-07-15 21:36:04 +0200

    Added Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,15 +16,27 @@ public class PlayerController : MonoBehaviour
     public GameObject Player;
 
     InputAction rotate;
+    InputAction move;
+
 
     [ShowNonSerializedField]
     Rotatation rotation;
+
+    [ShowNonSerializedField]
+    Movement movement;
 
     enum Rotatation
     {
         None,
         Clockwise,
         CounterClockwise
+    }
+
+    enum Movement
+    {
+        None,
+        Forwards,
+        Backwards
     }
 
     // Start is called before the first frame update
@@ -35,6 +47,29 @@ public class PlayerController : MonoBehaviour
         rotate = inputActionsMap.FindAction("Rotate");
         rotate.performed += OnRotate;
 
+        move = inputActionsMap.FindAction("Move");
+        move.performed += OnMove;
+
+    }
+
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+        var value = obj.ReadValue<float>();
+
+        if (value == 0)
+        {
+            movement = Movement.None;
+        }
+
+        if (value >  0)
+        {
+            movement = Movement.Forwards;
+        }
+
+        if (value <  0)
+        {
+            movement = Movement.Backwards;
+        }
 
 
     }
@@ -71,27 +106,44 @@ public class PlayerController : MonoBehaviour
         //The game might not have started, only process if the Player exists
         if (Player != null)
         {
-
-
-            var rangleToRotate = 0f;
+            var rotationAngleDelta = 0f;
             switch (rotation)
             {
                 case Rotatation.None:
                     break;
 
                 case Rotatation.Clockwise:
-                    rangleToRotate = PlayerSettings.RotationSpeedDegreesPerSecond * Time.deltaTime;
+                    rotationAngleDelta = PlayerSettings.RotationSpeedDegreesPerSecond * Time.deltaTime;
                     break;
 
                 case Rotatation.CounterClockwise:
-                    rangleToRotate = -PlayerSettings.RotationSpeedDegreesPerSecond * Time.deltaTime;
+                    rotationAngleDelta = -PlayerSettings.RotationSpeedDegreesPerSecond * Time.deltaTime;
                     break;
-
-
             }
 
             //Rotate the Player around the y-axis
-            Player.transform.Rotate(0, rangleToRotate, 0, Space.Self);
+            Player.transform.Rotate(0, rotationAngleDelta, 0, Space.Self);
+
+            var movementDelta = 0f;
+            switch (movement)
+            {
+                case Movement.None:
+                    break;
+
+                case Movement.Forwards:
+                    movementDelta = PlayerSettings.ForwardSpeedMetersPerSecond * Time.deltaTime;
+                    break;
+
+                case Movement.Backwards:
+                    movementDelta = -PlayerSettings.ForwardSpeedMetersPerSecond * Time.deltaTime;
+                    break;
+            }
+
+            //Move the Player in the z axis
+            Player.transform.Translate(new Vector3(0, 0, movementDelta), Space.Self);
+
+
+
 
         }
 
