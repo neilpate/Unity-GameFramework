395c557d3b844ae58b1ed79cac68c7dc26e02fc0
Neil <neil@premiergsolutions.com>
2021-09-11 22:24:12 +0200

    feat: Implemented Offsetting Checkerboard shader

---
+++ Assets/Prefabs/Munchy/ShaderController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    Material material;
+
+    public float Angle;
+
+    [SerializeField]
+    float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Angle += Speed * Time.deltaTime;
+
+        Angle = Angle % 360;
+        
+        material.SetFloat("Angle", Angle);
+    }
+}
