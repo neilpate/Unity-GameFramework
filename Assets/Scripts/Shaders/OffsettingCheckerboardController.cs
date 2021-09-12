0cc671d2edf9248b6ffdec7a8e4b740b74cc9fc2
Neil <neil@premiergsolutions.com>
2021-09-12 23:12:53 +0200

    feat: Border shader for Munchy

--- Assets/Scripts/Shaders/OffsettingCheckerboardController.cs
+++ Assets/Scripts/Shaders/OffsettingCheckerboardController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShaderController : MonoBehaviour
+public class OffsettingCheckerboardController : MonoBehaviour
 {
     Material material;
 
