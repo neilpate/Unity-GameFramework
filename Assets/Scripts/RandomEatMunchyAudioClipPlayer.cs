92cba611f251d6f4525cc7d1c8b09cbf4ce3f375
Neil <neil@premiergsolutions.com>
2021-09-18 14:01:26 +0200

    bugfix: Fixed munchy eating sound

--- Assets/Scripts/RandomEatMunchyAudioClipPlayer.cs
+++ Assets/Scripts/RandomEatMunchyAudioClipPlayer.cs
@@ -7,24 +7,17 @@ using UnityEngine;
 public class RandomEatMunchyAudioClipPlayer : MonoBehaviour
 {
     [SerializeField]
-    EatMunchy EatMunchy;
-
-    [SerializeField]
     List<AudioClip> AudioClips;
 
     AudioSource audioSource;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-      //  EatMunchy.MunchedEvent += OnMunched;
-
     }
 
-    private void OnMunched(int value)
+    public void PlaySound()
     {
         //Pick a random clip to play
         var index = UnityEngine.Random.Range(0, AudioClips.Count);
